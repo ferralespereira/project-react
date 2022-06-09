c5d5db6c067c4278e1723b1bc252f46b17e83601
javier ferrales <ferralespereira@gmail.com>
2022-06-09 18:36:32 -0400

    working with components

--- src/components/FirstComponent.js
+++ src/components/FirstComponent.js
@@ -1,7 +1,31 @@
 import React from 'react'
 
 export const FirstComponent = () => {
+
+    let name = "Javier";
+    let items = [
+        "item0",
+        "item1",
+        "item2",
+        "item3",
+        "item4",
+        "item5",
+    ]
+
   return (
-    <div>FirstComponent</div>
+    <div>
+        FirstComponent
+        <p>Mi name is: {name}</p>
+
+        <ul>
+            {
+                items.map( (item, index) => {
+                    return (<li key={index}>
+                        {item}
+                    </li>)
+                })
+            }
+        </ul>
+    </div>
   )
 }
